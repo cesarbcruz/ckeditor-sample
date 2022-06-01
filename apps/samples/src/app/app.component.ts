f6b25dd395728a204f4e3d85b1163f557c2bb445
Cesar Cruz <cesar.cruz@shift.com.br>
2022-06-01 17:35:17 -0300

    fix layout example

--- apps/samples/src/app/app.component.ts
+++ apps/samples/src/app/app.component.ts
@@ -42,4 +42,29 @@ export class AppComponent {
     console.log(event)
   }
 
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit(): void {
+    // store tabs variable
+    const myTabs = document.querySelectorAll("ul.nav-tabs > li");
+    function myTabClicks(tabClickEvent: { currentTarget: any; preventDefault: () => void; target: any; }) {
+      for (let i = 0; i < myTabs.length; i++) {
+        myTabs[i].classList.remove("active");
+      }
+      const clickedTab = tabClickEvent.currentTarget;
+      clickedTab.classList.add("active");
+      tabClickEvent.preventDefault();
+      const myContentPanes = document.querySelectorAll(".tab-pane");
+      for (let i = 0; i < myContentPanes.length; i++) {
+        myContentPanes[i].classList.remove("active");
+      }
+      const anchorReference = tabClickEvent.target;
+      const activePaneId = anchorReference.getAttribute("href");
+      const activePane = document.querySelector(activePaneId);
+      activePane.classList.add("active");
+    }
+    for (let i = 0; i < myTabs.length; i++) {
+      myTabs[i].addEventListener("click", myTabClicks)
+    }
+  }
+
 }
